e63dfc7636c33b87a530cd835b9c54ee2d059247
Salma <108466703+salmahmed6@users.noreply.github.com>
2025-10-28 12:04:31 +0300

    [136] [Time Beats: 100.00%] [Memory Beats: 83.27%] - LeetPush

--- Single-Number.cpp
+++ Single-Number.cpp
@@ -1,21 +1,31 @@
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         int n = nums.size();
+//         for(int i =0; i< n; i++){
+//             int num = nums[i];
+//             int count =0;
+
+//             for(int j =0; j< n; j++){
+//                 if(nums[j] == num){
+//                     count++;
+//                 }
+//             }
+//             if(count == 1) return num;
+//         }
+//         return -1;
+//     }
+// };
+
+
+//optimal solution using bitwise XOR
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int result =0; 
-        for(int num : nums){
-           result ^= num;
+        int result = 0;
+        for (int num : nums) {
+            result ^= num; // XOR cancels out duplicates
         }
         return result;
     }
 };
-
-
-
-
-// XOR of two same numbers is 0
-// XOR with 0 return number itself
-
-//approach for solving this ps
-// 1- initialize result =0
-// 2- iterate for the array and XOR each index with result
-// 3- result will hold the single number 
