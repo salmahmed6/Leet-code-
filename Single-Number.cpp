9eb896ad876b8d1e6ed4c9e1b93d221e6b8be323
Salma <108466703+salmahmed6@users.noreply.github.com>
2025-10-29 10:43:26 +0300

    [136] [Time Beats: 100.00%] [Memory Beats: 83.23%] - LeetPush

--- Single-Number.cpp
+++ Single-Number.cpp
@@ -1,31 +1,12 @@
-// class Solution {
-// public:
-//     int singleNumber(vector<int>& nums) {
-//         int n = nums.size();
-//         for(int i =0; i< n; i++){
-//             int num = nums[i];
-//             int count =0;
-
-//             for(int j =0; j< n; j++){
-//                 if(nums[j] == num){
-//                     count++;
-//                 }
-//             }
-//             if(count == 1) return num;
-//         }
-//         return -1;
-//     }
-// };
-
-
-//optimal solution using bitwise XOR
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int result = 0;
-        for (int num : nums) {
-            result ^= num; // XOR cancels out duplicates
+        int n = nums.size();
+        // XOR all the elements:
+        int xorr = 0;
+        for (int i = 0; i < n; i++) {
+            xorr = xorr ^ nums[i];
         }
-        return result;
+        return xorr;
     }
 };
